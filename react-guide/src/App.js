9a9eb62828e84a394dfc55f968ef7d2cb02411ee
JonathanAriass <UO283586@uniovi.es>
2022-05-01 22:00:29 +0200

    First component with JSX

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
+  // What we return here is a div for changing it on the root div
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- react-guide/src/index.js
+++ react-guide/src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// We are saying that the root render should be changed with App
+// this App which is a component.
 root.render(<App />);
