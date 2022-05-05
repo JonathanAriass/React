023bd3c108f8a0f05de0c5565002b7940b55a8c9
JonathanAriass <UO283586@uniovi.es>
2022-05-05 12:49:56 +0200

    Add expenses fail

--- react-guide/src/components/Expenses/Expenses.js
+++ react-guide/src/components/Expenses/Expenses.js
@@ -5,8 +5,7 @@ import ExpensesFilter from "./ExpensesFilter";
 import React, { useState } from "react";
 
 const Expenses = (props) => {
-
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -16,30 +15,20 @@ const Expenses = (props) => {
   return (
     <div>
       <Cards className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Cards>
     </div>
   );
-}
+};
 
 export default Expenses;
