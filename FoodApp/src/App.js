8ac00eb9b7d743a77229650555fac5953b7bbca4
Jonathan Arias Busto <UO283586@uniovi.es>
2022-06-16 21:49:24 +0200

    Finishing cart reducer

--- FoodApp/src/App.js
+++ FoodApp/src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/cart-provider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  // Every component need to get access to the provider so we need to wrap everything
   return (
-    <React.Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 }
 
